1b1a82e05ab238d7a4cef3a56193c3012b8347d0
adriano-fonseca <adriano-fonseca@PRO-PAE-4638.PROCERGS.REDERS>
2016-02-14 19:12:30 -0200

    Singleton and git ignore

---
+++ src/com/company/app/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package com.company.app.singleton;
+
+public class Singleton {
+  
+  private static Singleton singleton=null;
+  
+  public static Singleton getInstance() {
+    if(singleton==null){
+      singleton = new Singleton(); 
+    }
+
+    return singleton;
+  }
+}
